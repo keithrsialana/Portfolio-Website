3a05501454db46c7ec02c641847f29141ed552c9
Keith Sialana <ninjabaskaetball@hotmail.com>
2024-10-30 18:21:43 -0400

    added more components

--- src/components/projectElement.jsx
+++ src/components/projectElement.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const projectElement = () => {
+  const { title, description, imgUrl, gitHubUrl, deployedUrl } = props;
   return (
-    <div></div>
+    <div class="card">
+      <img src={imgUrl} class="card-img-top" alt={`${title} - Project Image`} />
+      <div class="card-body">
+        <h5 class="card-title">{title}</h5>
+        <p class="card-text">{description}</p>
+        <a href={gitHubUrl} className="btn btn-primary">Check the Code!</a>
+        {deployedUrl && (
+          <a href={deployedUrl} className="btn btn-primary">View the site!</a>
+        )}
+      </div>
+    </div>
   )
 }
 
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import ProjectElement from '../components/projectElement'
 const portfolio = () => {
   return (
-    <div>portfolio</div>
+    <div className="container-fluid">
+      <h1 className="text-center display-2">Here are some projects I've done!</h1>
+      <div class="card-group">
+        <ProjectElement title="" description="" imgUrl="" gitHubUrl="" deployedUrl="" />
+        <ProjectElement title="" description="" imgUrl="" gitHubUrl="" deployedUrl="" />
+        <ProjectElement title="" description="" imgUrl="" gitHubUrl="" deployedUrl="" />
+        <ProjectElement title="" description="" imgUrl="" gitHubUrl="" deployedUrl="" />
+        <ProjectElement title="" description="" imgUrl="" gitHubUrl="" deployedUrl="" />
+        <ProjectElement title="" description="" imgUrl="" gitHubUrl="" deployedUrl="" />
+      </div>
+    </div>
   )
 }
-
 export default portfolio
