830f70c1465e9bbf519e188c908ffe685cf32a03
Keith Sialana <ninjabasketball@hotmail.com>
2024-10-31 02:31:43 -0400

    completed contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,9 +1,138 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { IoMdMail } from "react-icons/io";
 const contact = () => {
-  return (
-    <div>contact</div>
-  )
-}
+	const [errorMessage, setErrorMessage] = useState("");
+	const [htmlError, setHTMLError] = useState("");
+	const [email, setEmail] = useState("");
+	const [name, setName] = useState("");
+	const [message, setMessage] = useState("");
 
-export default contact
+	function handleNameChange(e) {
+		setName(e.target.value);
+	}
+
+	function handleEmailChange(e) {
+		setEmail(e.target.value);
+	}
+
+	function handleMessageChange(e) {
+		setMessage(e.target.value);
+	}
+
+	useEffect(() => {
+		if (errorMessage) {
+			alert(errorMessage);
+			setErrorMessage("");
+		}
+	}, [errorMessage]);
+
+	function checkInput(e) {
+		// prevent page from reloading
+		e.preventDefault();
+
+		// check if valid email with regex
+		const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+		if (name == "" || message == "" || email == "") {
+			if (name == "") {
+				setErrorMessage("Your name is empty");
+				setHTMLError("Your name is empty");
+				return;
+			} else if (!emailPattern.test(email)) {
+				setErrorMessage("You have entered an invalid email address.");
+				setHTMLError("You have entered an invalid email address.");
+				return;
+			} else if (message == "") {
+				setErrorMessage("Your message is empty");
+				setHTMLError("Your message is empty");
+				return;
+			}
+		}
+
+		// send email
+		// Not Implemented
+
+		// reset form inputs
+		setHTMLError("");
+		setErrorMessage("");
+		setName("");
+		setMessage("");
+		setEmail("");
+
+		// email sent!
+		alert("Your email has been sent! 😊");
+	}
+
+	return (
+		<div className="text-center ">
+			<IoMdMail style={{ color: "#054BE0", fontSize: "5em" }} />
+			<h1 className="display-2">Contact Me</h1>
+			<p>
+				My preferred method of contact is through email. If you would like to
+				send me an email, you can use this form.
+			</p>
+			<form onSubmit={checkInput} className="mx-auto">
+				<div className="mb-3 row">
+					<label for="userName" className="col-sm-2 col-form-label">
+						<h3 className="">Name</h3>
+					</label>
+					<div className="col-sm-10">
+						<input
+							type="text"
+							className="form-control"
+							id="userName"
+							placeholder="John Doe"
+							value={name}
+							onChange={handleNameChange}
+						/>
+					</div>
+				</div>
+				<div className="mb-3 row">
+					<label for="staticEmail" className="col-sm-2 col-form-label">
+						<h3 className="">Email</h3>
+					</label>
+					<div className="col-sm-10">
+						<input
+							type="text"
+							className="form-control"
+							id="staticEmail"
+							placeholder="email@example.com"
+							value={email}
+							onChange={handleEmailChange}
+						/>
+					</div>
+				</div>
+				<div className="mb-3 row">
+					<label for="userMessage" className="col-sm-2 col-form-label">
+						<h3 className="">Message</h3>
+					</label>
+					<div className="col-sm-10">
+						<textarea
+							type="text"
+							className="form-control"
+							id="userMessage"
+							placeholder="What are your thoughts?"
+							rows="5"
+							value={message}
+							onChange={handleMessageChange}
+						/>
+					</div>
+				</div>
+				<div className="mb-3 row">
+					<div id="errorMessage" className="text-danger">
+						{htmlError}
+					</div>
+				</div>
+				<div className="mb-3 row">
+					<div className="col-sm-12">
+						<button type="submit" class="btn btn-primary mb-3 col-sm-6">
+							Send Email
+						</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default contact;
