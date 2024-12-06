eb725fce14ba6b3e264d36b89c361a66aac9eec8
Keith <34338831+keithrsialana@users.noreply.github.com>
2024-12-05 21:40:54 -0500

    added projects

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,5 +1,15 @@
 export const projects = [
     {
+        title: "Moody",
+        tech: "Typescript, SCSS, React, PostgreSQL",
+        description:
+            "This group project aims to use OpenAI API and Spotify API to collaborate with the user to generate a playlist based on the user's mood. The application will prompt the user to enter their mood, and will 'talk' to the OpenAI API to generate a list of 15 songs based on the user's mood. The deployed webpage will take a while to load, you might need to refresh the page a few times.",
+        imgUrl:
+            "https://cdn.pixabay.com/photo/2017/02/10/17/10/heart-2055695_960_720.png",
+        gitHubUrl: "https://github.com/keithrsialana/Moody",
+        deployedUrl: "https://moody-so0y.onrender.com",
+    },
+    {
         title: "HMRDiscordBot",
         tech: "Javascript",
         description:
@@ -8,6 +18,16 @@ export const projects = [
             "https://cdn.pixabay.com/photo/2024/03/11/19/15/ai-generated-8627457_960_720.png",
         gitHubUrl: "https://github.com/keithrsialana/HMRDiscordBot",
         deployedUrl: "",
+    },
+    {
+        title: "Hone Your Hollywood",
+        tech: "HTML, CSS, JavaScript",
+        description:
+            "This group project lets the user add their own list of movies that they would like to watch or re-watch, and have the app randomly pick between the movies in the list.",
+        imgUrl:
+            "https://cdn.pixabay.com/photo/2022/10/31/05/12/movie-theater-7558935_960_720.png",
+        gitHubUrl: "https://github.com/keithrsialana/hone-your-hollywood",
+        deployedUrl: "https://keithrsialana.github.io/hone-your-hollywood/",
     },
     {
         title: "Voice Assistant",
@@ -23,7 +43,7 @@ export const projects = [
         title: "Project-README",
         tech: "Javascript",
         description:
-            "A small application that generates a README.md file for the user after answering some prompts in a console",
+            "A small console application that generates a README.md file for the user after answering some prompts in a console",
         imgUrl:
             "https://cdn.pixabay.com/photo/2020/02/15/16/09/loveourplanet-4851331_960_720.jpg",
         gitHubUrl: "https://github.com/keithrsialana/Project-README",
