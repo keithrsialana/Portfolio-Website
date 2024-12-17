cdfd2e5e62c5328b4286bc36dc55df334f353faf
Keith <34338831+keithrsialana@users.noreply.github.com>
2024-12-17 12:02:09 -0500

    centered buttons

--- src/components/projectElem.jsx
+++ src/components/projectElem.jsx
@@ -11,17 +11,21 @@ const projectElem = (props) => {
 						<svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
 							<path />
 						</svg>
-						<img
-							class="card__thumb"
-							src="pfp.png"
-							alt=""
-						/>
+						<img class="card__thumb" src="pfp.png" alt="" />
 						<div class="card__header-text">
 							<h3 class="card__title">{title}</h3>
 							<span class="card__status">[ {tech} ]</span>
 						</div>
 					</div>
-                    {deployedUrl?<button className="btn bg-tomato" href={deployedUrl}>View Deployed Website</button>:<></>}
+					<div className="centered">
+						{deployedUrl ? (
+							<button className="btn bg-tomato" href={deployedUrl}>
+								View Deployed Website
+							</button>
+						) : (
+							<></>
+						)}
+					</div>
 					<p class="card__description">{description}</p>
 				</div>
 			</a>
